2cef83ddbebf3a7bc2ebcda4589adfc6ec70a5c5
dani <daniel.merino-cano@external.thalesaleniaspace.com>
2024-06-12 22:47:07 +0200

    Refactor the routes component

--- backend/app/routes/cat.py
+++ backend/app/routes/cat.py
@@ -1,30 +1,33 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from pymongo.database import Database
 
-from app.database import get_db
+from app.database import db_manager
 from app.models import Cat
 from app.schemas import CatSchema
 
 router = APIRouter()
 
 
-def get_cat_model():
-    db = get_db()
-    return Cat(db)
+def get_db() -> Database:
+    return db_manager.get_db()
 
 
 @router.post("/cats")
-def create_cat(cat: CatSchema, cat_model: Cat = Depends(get_cat_model)):
+async def create_cat(cat: CatSchema, db: Database = Depends(get_db)):
+    cat_model = Cat(db)
     cat_model.create_cat(cat.dict())
     return {"message": "Cat created successfully"}
 
 
 @router.get("/cats")
-def get_all_cats(cat_model: Cat = Depends(get_cat_model)):
+async def get_all_cats(db: Database = Depends(get_db)):
+    cat_model = Cat(db)
     return cat_model.get_all_cats()
 
 
 @router.get("/cats/{name}")
-def get_cat(name: str, cat_model: Cat = Depends(get_cat_model)):
+async def get_cat(name: str, db: Database = Depends(get_db)):
+    cat_model = Cat(db)
     cat = cat_model.get_cat(name)
     if cat is None:
         raise HTTPException(status_code=404, detail="Cat not found")
@@ -32,7 +35,8 @@ def get_cat(name: str, cat_model: Cat = Depends(get_cat_model)):
 
 
 @router.put("/cats/{name}")
-def update_cat(name: str, cat: CatSchema, cat_model: Cat = Depends(get_cat_model)):
+async def update_cat(name: str, cat: CatSchema, db: Database = Depends(get_db)):
+    cat_model = Cat(db)
     updated_cat = cat_model.update_cat(name, cat.dict())
     if updated_cat.modified_count == 0:
         raise HTTPException(status_code=404, detail="Cat not found")
@@ -40,7 +44,8 @@ def update_cat(name: str, cat: CatSchema, cat_model: Cat = Depends(get_cat_model
 
 
 @router.delete("/cats/{name}")
-def delete_cat(name: str, cat_model: Cat = Depends(get_cat_model)):
+async def delete_cat(name: str, db: Database = Depends(get_db)):
+    cat_model = Cat(db)
     deleted_cat = cat_model.delete_cat(name)
     if deleted_cat.deleted_count == 0:
         raise HTTPException(status_code=404, detail="Cat not found")

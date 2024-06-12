de2b1da76a4b6e06abae142c0f4818fe0c58922e
dani <daniel.merino-cano@external.thalesaleniaspace.com>
2024-06-12 23:58:18 +0200

    clear

--- frontend/cat-web-app/src/app/services/cat-api.service.ts
+++ frontend/cat-web-app/src/app/services/cat-api.service.ts
@@ -8,27 +8,27 @@ import { Cat } from '../model/cat.model';
 })
 export class CatService {
     // TODO replace with /api/cat and add proxy.conf.json
-    private apiUrl = 'http://localhost:8000';
+    private CAT_API_URI = 'http://localhost:8000';
 
     constructor(private http: HttpClient) { }
 
     getCats(): Observable<Cat[]> {
-        return this.http.get<Cat[]>(`${this.apiUrl}/cats`);
+        return this.http.get<Cat[]>(`${this.CAT_API_URI}/cats`);
     }
 
     getCat(name: string): Observable<Cat> {
-        return this.http.get<Cat>(`${this.apiUrl}/cats/${name}`);
+        return this.http.get<Cat>(`${this.CAT_API_URI}/cats/${name}`);
     }
 
     createCat(cat: Cat): Observable<any> {
-        return this.http.post(`${this.apiUrl}/cats`, cat);
+        return this.http.post(`${this.CAT_API_URI}/cats`, cat);
     }
 
     updateCat(name: string, cat: Cat): Observable<any> {
-        return this.http.put(`${this.apiUrl}/cats/${name}`, cat);
+        return this.http.put(`${this.CAT_API_URI}/cats/${name}`, cat);
     }
 
     deleteCat(name: string): Observable<any> {
-        return this.http.delete(`${this.apiUrl}/cats/${name}`);
+        return this.http.delete(`${this.CAT_API_URI}/cats/${name}`);
     }
 }

a4f0778882203d7bf335a4b764a22f349b27a65c
dani <daniel.merino-cano@external.thalesaleniaspace.com>
2024-06-12 22:44:15 +0200

    Refacotr database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,21 +1,31 @@
+from typing import Optional
+
 from pymongo import MongoClient
-
-client = None
-
-
-def connect_to_db():
-    global client
-    client = MongoClient("mongodb://localhost:27017")
+from pymongo.database import Database
 
 
-def close_db_connection():
-    global client
-    if client is not None:
-        client.close()
+class MongoDBClientManager:
+    def __init__(self, uri: str, db_name: str):
+        self.uri = uri
+        self.db_name = db_name
+        self.client: Optional[MongoClient] = None
+        self.db: Optional[Database] = None
+
+    def connect(self):
+        if self.client is None:
+            self.client = MongoClient(self.uri)
+            self.db = self.client[self.db_name]
+
+    def close(self):
+        if self.client:
+            self.client.close()
+            self.client = None
+            self.db = None
+
+    def get_db(self) -> Database:
+        if self.client is None:
+            self.connect()
+        return self.db
 
 
-def get_db():
-    global client
-    if client is None:
-        connect_to_db()
-    return client["cat_db"]
+db_manager = MongoDBClientManager(uri="mongodb://localhost:27017", db_name="cat_db")

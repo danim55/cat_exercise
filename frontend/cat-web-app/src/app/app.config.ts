28d7f44e3783d9b287dbafe8dd5b37f2c56f1efe
dani <daniel.merino-cano@external.thalesaleniaspace.com>
2024-06-21 17:47:10 +0200

    Add material to project

--- frontend/cat-web-app/src/app/app.config.ts
+++ frontend/cat-web-app/src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(
       withFetch(),
-    ),
+    ), provideAnimationsAsync(),
   ],
 
 };
